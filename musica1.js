3f5255ced84d4daedf348ab2ba8625d992171499
BrunoT <brunovdlc@hotmail.com>
2024-01-26 17:47:53 -0300

    Update musica1.js

--- musica1.js
+++ musica1.js
@@ -15,7 +15,13 @@ $('.slider').slick({
 var sources = [
     'audio1.mp3',
     'audio2.mp3',
-    'audio3.mp3'
+    'audio3.mp3',
+    'audio4.mp3',
+    'audio5.mp3',
+    'audio6.mp3',
+    'audio7.mp3',
+    'audio8.mp3',
+    'audio9.mp3'
 ];
 
 // Configurando o objeto Audio
